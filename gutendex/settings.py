4f2befc3217d4f48e61234491a7e6621ea623527
jitash <jitash@foxmail.com>
2025-10-27 01:26:58 +0800

    change databse config for Zeabur

--- gutendex/settings.py
+++ gutendex/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import environ
 import os
+import dj_database_url
 
 
 # Import environment variables
@@ -106,7 +107,11 @@ WSGI_APPLICATION = 'gutendex.wsgi.application'
 #         'PORT': env('DATABASE_PORT'),
 #     }
 # }
-DATABASES = {'default': env.db('DATABASE_URL')}
+
+# DATABASES = {'default': env.db('DATABASE_URL')}
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 SECRET_KEY = env('SECRET_KEY')
 
 
